f61d8d78a317b9b04117d7417da4348642eaa0b7
Dominika Włodarczyk <dominikaa.99@gmail.com>
2020-09-08 20:21:16 +0200

    Add initAccordion functionality

--- src/js/script.js
+++ src/js/script.js
@@ -59,6 +59,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
 
@@ -67,14 +68,49 @@
 
       /*generate HTML based on template*/
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      /*create elemtns using utils.createElementFromHTML */
+      /*create elements using utils.createElementFromHTML */ 
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       /*find menu container*/
       const menuContainer = document.querySelector(select.containerOf.menu);
       /*add element to menu*/
       menuContainer.appendChild(thisProduct.element);
     }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log(clickableTrigger);
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function(){
+        console.log('clicked');
+        /* prevent default action for event */
+        event.preventDefault();
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.add(classNames.menuProduct.wrapperActive);
+        /* find all active products */
+        const allActiveProducts = document.querySelectorAll(select.all.menuProductsActive);
+        console.log(allActiveProducts);
+        /* START LOOP: for each active product */
+        for (let activeProduct of allActiveProducts){
+          console.log(activeProduct == thisProduct.element);
+          /* START: if the active product isn't the element of thisProduct */
+          if (thisProduct.element != activeProduct){
+            /* remove class active for the active product */
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+          /* END: if the active product isn't the element of thisProduct */
+          }
+        /* END LOOP: for each active product */
+        }
+      /* END: click event listener to trigger */
+      });
+    }
   }
+
+
+  
+
 
   const app = {
     initMenu: function(){
